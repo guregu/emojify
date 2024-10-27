aaae1205fdea6aa8faa64582e50c93bb4c434645
guregu <greg@toki.waseda.jp>
2024-10-28 08:52:40 +0900

    update examples, go mod tidy

--- example_test.go
+++ example_test.go
@@ -1,17 +1,23 @@
 package emojify_test
 
 import (
+	"bytes"
+	"fmt"
 	"html/template"
 	"os"
 
 	"github.com/guregu/emojify"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func ExampleHTML() {
+	twemoji := emojify.New(emojify.WithCDN("https://twemoji.example.com/assets/"))
+
 	tmpl := template.New("")
 	// Install our emojifying function as "emojify"
 	tmpl.Funcs(template.FuncMap{
-		"emojify": emojify.HTML,
+		"emojify": twemoji.HTML,
 	})
 
 	// Simple example template where we'll emojify the title and body text of an article
@@ -28,5 +34,33 @@ func ExampleHTML() {
 	if err := tmpl.Execute(os.Stdout, data); err != nil {
 		panic(err)
 	}
-	// Output: <article><h1>hello <img draggable="false" class="emoji" src="https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.1.0/assets/svg/1f30e.svg" width="72" height="72" alt="🌎"/></h1><p>no <img draggable="false" class="emoji" src="https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.1.0/assets/svg/1f6ab.svg" width="72" height="72" alt="🚫"/> javascript for me <img draggable="false" class="emoji" src="https://cdn.jsdelivr.net/gh/jdecked/twemoji@15.1.0/assets/svg/1f606.svg" width="72" height="72" alt="😆"/></p></article>
+	// Output: <article><h1>hello <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f30e.svg" width="72" height="72" alt="🌎"/></h1><p>no <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f6ab.svg" width="72" height="72" alt="🚫"/> javascript for me <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f606.svg" width="72" height="72" alt="😆"/></p></article>
+}
+
+func ExampleReplaceHTML() {
+	twemoji := emojify.New(emojify.WithCDN("https://twemoji.example.com/assets/"))
+
+	// first, we render markdown to html
+	rendered := someMarkdownLibrary([]byte("# hello 🌎\n\nno 🚫 javascript for me 😆"), nil, nil)
+	// then, parse the html
+	body := &html.Node{Type: html.ElementNode, Data: "body", DataAtom: atom.Body}
+	frags, err := html.ParseFragment(bytes.NewReader(rendered), body)
+	if err != nil {
+		panic(err)
+	}
+	// replace text elements with emoji in them
+	var buf bytes.Buffer
+	for _, frag := range frags {
+		twemoji.ReplaceHTML(frag)
+		if err := html.Render(&buf, frag); err != nil {
+			panic(err)
+		}
+	}
+	// our emojified HTML:
+	fmt.Println(buf.String())
+	// Output: <h1><span>hello <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f30e.svg" width="72" height="72" alt="🌎"/></span></h1><p><span>no <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f6ab.svg" width="72" height="72" alt="🚫"/> javascript for me <img draggable="false" class="emoji" src="https://twemoji.example.com/assets/svg/1f606.svg" width="72" height="72" alt="😆"/></span></p>
+}
+
+func someMarkdownLibrary(_ []byte, _, _ any) []byte {
+	return []byte(`<h1>hello 🌎</h1><p>no 🚫 javascript for me 😆</p>`)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/guregu/emojify
 
 go 1.23.1
 
-require (
-	golang.org/x/net v0.30.0
-)
+require golang.org/x/net v0.30.0
